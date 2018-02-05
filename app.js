0b79e2986054ac68235bc01babcc8bdc96fd9605
portarul23 <blanarugeorge96@gmail.com>
2018-02-05 15:12:01 +0000

    Taking questions from DB

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//old way of writting the server
+//old way of setting up a node server
 //var app = require('express')();
 //var http = require('http').Server(app);
 //var io = require('socket.io')(http);
@@ -12,62 +12,53 @@ var server = http.createServer(app);
 var io = require('socket.io')(server);
 var port = 3000;
 
+var questionAndAnswerArray = [];
 
 var sqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'irishq'
+    database: 'irish-conq'
 });
 sqlConnection.connect(function (err) {
-
-    if (err) {
-        throw err;
-    } else {
-        console.log("connected");
-    }
-
+    if (err) throw err;
+    sqlQuery = "SELECT * FROM questiontable as a,answertableexample as b WHERE a.question_id = b.questionId and a.question_id = 1";
+    sqlConnection.query(sqlQuery, function (error, results, fields) {
+        if (error) throw error;
+        questionAndAnswerArray = results;
+    });
+    //    console.log("connected");
 });
-
-var questionTable;
-
-questionTable = sqlConnection.query('SELECT * FROM questiontable', function (error, results, fields) {
-    if (error) throw error;
-//    console.log(results);
-//    socket.emit('questions array', results);
-//    console.log("Question table event sent");
-    return results;
-});
-
-console.log(questionTable);
-
 
 app.use(express.static("./public"));
 
-
+var i = 0;
 io.on('connection', function (socket) {
+
+    //test to see if the connection is estabilished 
+    //to be edited to give each user an unique id
     io.emit('conn');
     socket.on('disconnect', function () {
         io.emit('disc');
     });
-    //    socket.on('chat msg', function (msg) {
-    //        console.log('message:' + msg);
-    //        socket.broadcast.emit('chat msg', msg);
-    //    });
-    //    socket.on('user typing', function (username) {
-    //        console.log(username + " is typing");
-    //        io.emit('user typing', username);
-    //    });
-    //    socket.on('user not typing', function () {
-    //        io.emit('user not typing');
-    //    });
 
+
+    socket.on('request random question', function () {
+        socket.emit('random question', questionAndAnswerArray);
+
+
+    });
+
+
+    //this method runs multiple times, for some reason //doesnt cause any trouble yet
     socket.on('right answer', function (ter) {
         console.log(ter);
         console.log("Right answer event received");
         socket.broadcast.emit('right answer', ter);
         console.log("Right answer event sent");
     });
+
+
 });
 
 
@@ -78,10 +69,19 @@ io.on('connection', function (socket) {
 
 
 
-
-
-
-//start the on selected port! in this case 3000
+//start the server on selected port! in this case 3000
 server.listen(port, function () {
     console.log('listening on *:' + port);
 });
+//Math.floor(Math.random()*100%50)
+//    socket.on('chat msg', function (msg) {
+//        console.log('message:' + msg);
+//        socket.broadcast.emit('chat msg', msg);
+//    });
+//    socket.on('user typing', function (username) {
+//        console.log(username + " is typing");
+//        io.emit('user typing', username);
+//    });
+//    socket.on('user not typing', function () {
+//        io.emit('user not typing');
+//    });
